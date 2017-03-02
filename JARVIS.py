ed7f3afb2416b046b596a32470c50c23bd109e3c
Cole Anthony Capilongo <colecmac@gmail.com>
2017-03-02 17:59:38 -0500

    changed intro and var intialisation

--- JARVIS.py
+++ JARVIS.py
@@ -1,7 +1,9 @@
 import webbrowser
 
+epassword = None
+
 while epassword != 13: # breaks out of the loop when the password is right
-    print('Welcome to:\nJ  A  R  V  U  S\nPlease log in: ')
+    print('Welcome to:\nJ  A  R  V  I  S\nPlease log in: ')
     eusername = input('Please enter your username:')
     if eusername == 'Justin':
         epassword = input('Please enter your password:')
