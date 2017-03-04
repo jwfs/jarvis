e158088f7a14e106903a85596ca8521aa10ea70c
jwfs <jwfs2003@gmail.com>
2017-03-03 20:36:27 -0500

    made login function so loops could be made for certian commands
    logout and bye specifically

--- JARVIS.py
+++ JARVIS.py
@@ -1,28 +1,31 @@
 import webbrowser
 
-epassword = None
+def login():
+    epassword = None
+    while epassword != '13': # breaks out of the loop when the password is right
+        eusername = input('Please enter your username:')
+        if eusername == 'Justin':
+            epassword = input('Please enter your password:')
 
-print('Welcome to:\nJ  A  R  V  I  S\nPlease log in: ')
-
-while epassword != '13': # breaks out of the loop when the password is right
-    eusername = input('Please enter your username:')
-    if eusername == 'Justin':
-        epassword = input('Please enter your password:')
-
-print('Hello,', eusername)
-
-while True:
-    user = input('Enter Command: ')
-    if user == 'hi':
-        print('hi')
-    elif user == 'bye':
-        print('bye')
-        break
-    elif user == 'logout':
-        print('logging out')
-        break
-    elif user == 'chrome':
-        csearch = input('Search on google: ')
-        webbrowser.open_new_tab('https://www.google.com/search?q=' + csearch)
+running = True
+while running:
+    print('Welcome to:\nJ  A  R  V  I  S\nPlease log in: ')
+    login()
+    print('Hello,', eusername)
+    login = True
+    while login:
+        user = input('Enter Command: ')
+        if user == 'hi':
+            print('hi')
+        elif user == 'bye':
+            print('bye')
+            login = False
+            running = False
+        elif user == 'logout':
+            print('logging out')
+            break
+        elif user == 'chrome':
+            csearch = input('Search on google: ')
+            webbrowser.open_new_tab('https://www.google.com/search?q=' + csearch)
 
 print('Jarvis has closed and you have been logged out.')
